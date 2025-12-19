76ad453f5205397a50cb9922d80248ebcdffd6f8
Fayziev Muminjon <fayzievmuminjon@gmail.com>
2025-12-19 09:57:09 +0500

    create call link from phone icon on left bottom side

--- wp-content/themes/ant-preserved/footer.php
+++ wp-content/themes/ant-preserved/footer.php
@@ -1,7 +1,7 @@
 </main>
 <?php get_template_part('template-parts/footer'); ?>
-<button class="sl-button _primary_pubuv_31 _button_m9inp_1" type="button"><span><svg fill="none" height="24" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_4023_147)"><path d="M5 4H9L11 9L8.5 10.5C9.57096 12.6715 11.3285 14.429 13.5 15.5L15 13L20 15V19C20 19.5304 19.7893 20.0391 19.4142 20.4142C19.0391 20.7893 18.5304 21 18 21C14.0993 20.763 10.4202 19.1065 7.65683 16.3432C4.8935 13.5798 3.23705 9.90074 3 6C3 5.46957 3.21071 4.96086 3.58579 4.58579C3.96086 4.21071 4.46957 4 5 4Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path></g><defs><clippath id="clip0_4023_147"><rect fill="white" height="24" width="24"></rect></clippath></defs></svg></span>
-</button>
+<a href="tel:446302023" class="sl-button _primary_pubuv_31 _button_m9inp_1" type="button"><span><svg fill="none" height="24" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_4023_147)"><path d="M5 4H9L11 9L8.5 10.5C9.57096 12.6715 11.3285 14.429 13.5 15.5L15 13L20 15V19C20 19.5304 19.7893 20.0391 19.4142 20.4142C19.0391 20.7893 18.5304 21 18 21C14.0993 20.763 10.4202 19.1065 7.65683 16.3432C4.8935 13.5798 3.23705 9.90074 3 6C3 5.46957 3.21071 4.96086 3.58579 4.58579C3.96086 4.21071 4.46957 4 5 4Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path></g><defs><clippath id="clip0_4023_147"><rect fill="white" height="24" width="24"></rect></clippath></defs></svg></span>
+</a>
 </div>
 </div>
 
